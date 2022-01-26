6db4ffc98938d6702cf7233b61d68a10ef6f1984
Kenned Ferreira <kenned14santos@gmail.com>
2022-01-25 22:41:25 -0200

    Adicionado alguns efeitos no bg
    quadrados subindo

--- portifolio.js
+++ portifolio.js
@@ -1,3 +1,27 @@
 
 
-alert('eita');
+function shuffleName(name){
+    
+    name.innerHTML = shuffleString(name.innerHTML);
+    //alert("botao");
+}
+
+function shuffleString(str){
+    let arr = str.split('');
+    arr = arr.map((elem)=>{
+        return String.fromCharCode(97+Math.random()*25);
+    });
+    
+    arr[0]=arr[0].toUpperCase();
+    arr[1] = '.';
+    return arr.join('');
+}
+
+
+let name = document.getElementById('name');
+let shuffleButton = document.getElementById('shuffle');
+
+console.log(shuffleButton);
+shuffleButton.addEventListener("click", function(){
+    shuffleName(name)
+});
