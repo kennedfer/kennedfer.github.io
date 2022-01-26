f20ba1483ebfe2245c615a74d6d91fd99a9dc013
Kenned Ferreira <kenned14santos@gmail.com>
2022-01-25 22:49:46 -0200

    testando apenas

--- portifolio.js
+++ portifolio.js
@@ -1,27 +1,3 @@
 
 
-function shuffleName(name){
-    
-    name.innerHTML = shuffleString(name.innerHTML);
-    //alert("botao");
-}
-
-function shuffleString(str){
-    let arr = str.split('');
-    arr = arr.map((elem)=>{
-        return String.fromCharCode(97+Math.random()*25);
-    });
-    
-    arr[0]=arr[0].toUpperCase();
-    arr[1] = '.';
-    return arr.join('');
-}
-
-
-let name = document.getElementById('name');
-let shuffleButton = document.getElementById('shuffle');
-
-console.log(shuffleButton);
-shuffleButton.addEventListener("click", function(){
-    shuffleName(name)
-});
+alert("js funcionando");
